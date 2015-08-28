748c0ff30d8f5505c8b16436f164a10b2774d0a0
Dylan <invision33@gmail.com>
2015-08-28 08:57:57 -0500

    added questions and user models

---
+++ db/migrate/20150828135725_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :api_key
+
+      t.timestamps null: false
+    end
+  end
+end
