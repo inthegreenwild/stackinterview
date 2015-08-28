2440d89170d5b2d5a85bb1d372f8adb9746fa7aa
Dylan <invision33@gmail.com>
2015-08-28 10:04:42 -0500

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'user/new'
+
+  get 'user/create'
+
   namespace :api, defaults: {format: 'json'} do 
     namespace :v1 do 
       resources :questions, except: :destroy 
