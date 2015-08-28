0f2ab85f8a7fbab2b198c7ee3030c468810abc95
Dylan <invision33@gmail.com>
2015-08-28 09:27:31 -0500

    add json response to questions controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,16 +1,28 @@
 class Api::V1::QuestionsController < ApplicationController
+  respond_to :json 
+
   def index
+    respond_with Question.all 
   end
 
   def show
+    respond_with Question.find(params[:id])
   end
 
   def edit
+
   end
 
   def create
+    respond_with Question.create(question_params)
   end
 
   def update
+    respond_with Question.update(question_params)
   end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :answer, :rating, :category)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-  namespace :v1 do
-    get 'questions/index'
-    end
-  end
-
-  namespace :api do
-  namespace :v1 do
-    get 'questions/show'
-    end
-  end
-
-  namespace :api do
-  namespace :v1 do
-    get 'questions/edit'
-    end
-  end
-
-  namespace :api do
-  namespace :v1 do
-    get 'questions/create'
-    end
-  end
-
-  namespace :api do
-  namespace :v1 do
-    get 'questions/update'
-    end
-  end
-
   namespace :api do 
     namespace :v1 do 
       resources :questions, except: :destroy 
