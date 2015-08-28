85d76687c269ae786694efaed23531f7cb49caa2
Dylan <invision33@gmail.com>
2015-08-28 10:10:51 -0500

    update user controller and view for signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,5 +3,18 @@ class UserController < ApplicationController
   end
 
   def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		session[:user_id] = @user.id 
+  		redirect_to '/'
+  	else 
+  		flash[:error] = 'An error has occured :('
+  		render 'new'
+  	end 
   end
+
+  private 
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password)
+  	end 
 end
